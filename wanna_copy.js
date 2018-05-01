88cc48daa3a3da8400afe5bbf95e5665e128958d
Maz <massys85@gmail.com>
2018-05-01 17:55:23 +0200

    Initial commit

---
+++ wanna_copy.js
@@ -0,0 +1,17 @@
+/* This version does not allow multiple selection, more "user-friendly" though. */
+
+let child = document.querySelector('.post').children, s = false 
+       
+for(let i = 0; i < child.length; i++){
+    child[i].onclick = function(){
+       if(!s){
+            let range = document.createRange();
+            range.selectNode(this);
+            window.getSelection().addRange(range);
+            s = true
+        } else {
+            window.getSelection().removeAllRanges();
+            s = false
+        }
+    }
+}
